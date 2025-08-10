26f1249bb3d3f33a246828fa7a367e9e569baf38
OnesmusKirimi <onesmuskirimi64@gmail.com>
2025-08-11 00:13:08 +0300

    added port number for the backend to work

--- server/index.js
+++ server/index.js
@@ -153,6 +153,6 @@ io.on("connection", async (socket) => {
 
 // Final: start the server
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
